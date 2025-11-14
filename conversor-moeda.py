6ba17ab68a9c2d61e6e998e3a53636d93a468b33
Matheus Lima <matheus.lima2017@outlook.com>
2025-11-14 16:31:33 -0300

    Delete conversor-moeda.py

--- conversor-moeda.py
+++
@@ -1,5 +0,0 @@
-# MAIS UM DESAFIO GUNABARA, PROGRMA DE CONVERSOR DE MOEDA, REAL---->DOLAR
-r=float(input('Quanto vc tem na carteira ?'))
-d = r/5.50
-print('Com {:.2f}, voce tem {:.2f} dolares.'.format(r,d))
-#{:.2f} significa que só quero duas casas depois da vírgula, se chama 2 casas apos a casa flutuante.
